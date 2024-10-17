0d31040c03d6013f7044efb932ba578b9562f836
Antonio Rodríguez Sánchez <antoniors05.ars@gmail.com>
2024-10-17 08:44:38 +0200

    Suma

--- src/capitulo1/Calculadora.java
+++ src/capitulo1/Calculadora.java
@@ -5,6 +5,7 @@ public class Calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Calculadora");
+		System.out.println("Suma");
 	}
 
 }
